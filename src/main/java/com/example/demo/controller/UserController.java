bce9d6363539af4c474bf3c785d781e871eebada
Choi <hungmuoichoi102@gmail.com>
2026-03-14 09:10:21 +0700

    xdpmWeb-cmt5

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,9 @@ public class UserController {
     @GetMapping("/users") // Kết hợp lại thành /BASE_API/users
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+    @GetMapping("/{id}") // Kết hợp lại thành /BASE_API/{id}
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElse(null);
     }
 }
