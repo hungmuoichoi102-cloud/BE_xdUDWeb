37fd84d566510b7722b1054e35a937f229ed1244
Choi <hungmuoichoi102@gmail.com>
2026-04-06 17:40:49 +0700

    fix bug1

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -4,9 +4,21 @@ import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,9 +29,19 @@ public class AuthController {
     private UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthRequest authRequest) {
+    public ResponseEntity<?> login(@RequestBody AuthRequest authRequest, HttpServletRequest request) {
         if (userService.validateUserCredentials(authRequest.getUsername(), authRequest.getPassword())) {
             User user = userService.getUserByUsername(authRequest.getUsername());
+            String roleName = user.getRole().toUpperCase();
+            if (!roleName.startsWith("ROLE_")) {
+                roleName = "ROLE_" + roleName;
+                }
+            List<SimpleGrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority(roleName));
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                authRequest.getUsername(), authRequest.getPassword(), authorities);
+            SecurityContextHolder.getContext().setAuthentication(token);
+            HttpSession session= request.getSession(true);
+            session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
             return ResponseEntity.ok(new AuthResponse("Login successful", user));
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
