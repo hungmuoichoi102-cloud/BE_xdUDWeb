ae227c567a9366022960343d6d603dd085405700
Choi <hungmuoichoi102@gmail.com>
2026-04-09 13:14:43 +0700

    fix bug2

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -11,7 +11,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String name;
 
     @Column(nullable = false)
